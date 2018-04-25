78b927cd512302834b572435d30505fead4b21b2
minhhienpham <minhhienphamcntt@gmail.com>
2018-04-25 22:39:16 +0700

    create feedback form

---
+++ views/site/feedback.php
@@ -0,0 +1,23 @@
+<?php
+$this->title = 'Feedback';
+?>
+<div class="form-group row">
+		<label class="col-md-3 control-label">Email:</label>
+		<div class="col-md-9">
+			<input class="form-control"></input>
+		</div>
+	</div>
+
+	<div class="form-group row">
+		<label class="col-md-3 control-label">Response Message:</label>
+		<div class="col-md-9">
+			<textarea
+				style="height: 100px; width: 100%;">
+			</textarea>
+		</div>
+	</div>
+
+	<p align="center">
+		<button type="submit" class="btn btn-default">SEND</button>
+	</p>
+</div>
