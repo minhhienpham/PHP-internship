b52de7a50426d33a059ec2047e180c3de5cd02f1
minhhienpham <minhhienphamcntt@gmail.com>
2018-04-26 13:35:29 +0700

    update feedback form

--- views/feedback/_form.php
+++ views/feedback/_form.php
@@ -17,7 +17,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'message')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Send', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
